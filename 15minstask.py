b8732b754cac99c0f5b86064b91bb07344d0aec5
Ruthrakotti42 <138431151+Ruthrakotti42@users.noreply.github.com>
2023-07-13 10:45:05 +0530

    updated

---
+++ 15minstask.py
@@ -0,0 +1,19 @@
+text=input("Enter to write some in file")
+x=open('15minstask.txt','w')
+x.write(text)
+x.close()
+
+import smtplib
+
+sender_email = "ruthrakotti777@gmail.com"
+password = "ntvndudyompuleeo"
+to_email = "veera4012@gmail.com"
+server = smtplib.SMTP("smtp.gmail.com",587)
+server.starttls()
+server.login(sender_email, password)
+print("login successful")
+server.sendmail(sender_email,to_email,text)
+print("email sent to ",to_email)
+server.quit()
+
+
